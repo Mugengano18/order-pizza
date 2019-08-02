9bac12bd67bc75c39edc78de5832db04434bf000
Mugengano18 <mugenganoa@gmail.com>
2019-08-02 19:24:43 +0200

    I added if statements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,633 @@
-//bussiness logic
+// business logic
 function order(size, crust, topping) {
-    this.size = size;
-    this.crust = crust;
-    this.topping = topping;
+    this.size =size;
+    this.crust =crust;
+    this.topping =topping;
+ }
+var s = ["small", "Medium", "large"];
+var c = ["New england style", "grilled", "deep dish", "stuffed", "cast iron", "flat bread"];
+var t = ["Sausage, caramelized onions, and fennel", "Pepperoni, tomatoes, mushrooms, and onion", "Pepperoni, sausage, green pepper, onion, and mushroom", "Grilled chicken, avocado, and cherry tomatoes", "Roasted Brussels sprouts and bacon or pancetta", "Sardines, red onions, and black olives", "Pesto with white beans, tomato, arugula, and Parmesan cheese", "Goat cheese with arugula and red onion", "Fried eggplant slices and black olives"]
+
+
+var sprice, cprize, tprice,totalPrice;
+function totalprice(size, crust, top) {
+    if (size === s[0]) {
+        sprice = 1000;
+        if (crust === c[0]) {
+            cprize = 300
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+
+        }
+        else if (crust === c[1]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[2]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[3]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[4]) {
+            cprize = 500;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[5]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+
+
+    }
+    if (size === s[1]) {
+        sprice = 1500;
+        if (crust === c[0]) {
+            cprize = 300
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+
+        }
+        else if (crust === c[1]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[2]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[3]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[4]) {
+            cprize = 500;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[5]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+
+
+    }
+    if (size === s[2]) {
+        sprice = 2000;
+        if (crust === c[0]) {
+            cprize = 300
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+
+        }
+        else if (crust === c[1]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[2]) {
+            cprize = 400;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[3]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[4]) {
+            cprize = 500;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+        else if (crust === c[5]) {
+            cprize = 300;
+            if (top === t[0]) {
+                tprice = 500
+            }
+            else if (top === t[1]) {
+                tprice = 600;
+            }
+            else if (top === t[2]) {
+                tprice = 800;
+            }
+            else if (top === t[3]) {
+                tprice = 800;
+            }
+            else if (top === t[4]) {
+                tprice = 800;
+            }
+            else if (top === t[5]) {
+                tprice = 800;
+            }
+            else if (top === t[6]) {
+                tprice = 800;
+            }
+            else if (top === t[7]) {
+                tprice = 800;
+            }
+            else if (top === t[8]) {
+                tprice = 800;
+            }
+            else if (top === t[9]) {
+                tprice = 800;
+            }
+        }
+
+
+    }
+    totalPrice=sprice+cprize+tprice;
+    return  totalPrice;
 }
+console.log(totalprice("small","grilled","Sausage, caramelized onions, and fennel"))
+
 
 
 
@@ -24,17 +648,17 @@ $(document).ready(function () {
         $(".pizzaf").append('<div class="form-group">' +
             '<label for="size">Choose any size:</label>' +
             '<select class="form-control" id="select1">' +
-            '<option>Small #600rwf</option>' +
-            '<option>Medium #800rwf</option>' +
-            '<option>Large #1000rwf</option>' +
+            '<option>Small</option>' +
+            '<option>Medium</option>' +
+            '<option>Large</option>' +
             '</select>' +
             '</div>' +
             '<div class="form-group">' +
             '<label for="sel2">choose the crust:</label>' +
             '<select class="form-control" id="select2">' +
-            '<option>New England Greek Style #100rwf</option>' +
-            '<option>Grilled #300rwf</option>' +
-            '<option>Deep Dish #200rwf</option>' +
+            '<option>New England Greek Style</option>' +
+            '<option>Grilled</option>' +
+            '<option>Deep Dish</option>' +
             '<option>Stuffed</option>' +
             '<option>Cast Iron</option>' +
             '<option>Flatbread</option>' +
@@ -64,19 +688,20 @@ $(document).ready(function () {
     })
     $("#check").click(function () {
         var inputsize = $("#select1").val();
-        alert(inputsize);
+        console.log(inputsize);
         var inputcrust = $("#select2").val();
-        alert(inputcrust);
+        console.log(inputcrust);
         var inputtopping = $("#select3").val();
-        alert(inputtopping);
+        console.log(inputtopping);
         var newOrder = new order(inputsize, inputcrust, inputtopping);
 
-        alert(newOrder);
+
     })
-    $("#send").click(function(){
+    $("#send").click(function () {
         alert("thank you!!!you'll get the pizza in no time")
     })
-    $("#submit").click(function(){
+    $("#submit").click(function () {
         alert("Thank you!!!")
     })
 })
+
